e73a2009f380c65d70927a0b8facea07de1d78ce
KillerBee3012 <33883984+KillerBee3012@users.noreply.github.com>
2018-01-16 10:33:49 +0800

    Updated

--- Exercise-Function-2.cpp
+++ Exercise-Function-2.cpp
@@ -1,9 +1,33 @@
-//this is an unfinish program
-//finish up this program
+//Aidil Suffian Bin Abu Bakar (A17DW2253)
+//updated
+
+#include <iostream>
+
+using namespace std;
+
+int x,y,result;
 
 int sum(int x,int y)
 {
 	int result;
 	result = x+y;
 
+}
+
+int main ()
+
+{
+
+	
+	cout<< "Please Enter First Number To Add"<<endl;
+	cin>> x;
+	
+	cout<<"Please Enter The Second Number To Add"<<endl;	
+	cin>> y;
+	
+	cout<< "Your Total Number : " <<sum(x,y)<<endl<<endl;
+	
+	cout<< "Thank You";
+	
+	
 }
